589215697de61efac04978d8fdff56b403c9a6b4
dstearle <dallas.st.earle@gmail.com>
2021-09-17 13:22:17 -0700

    Added mutations to the vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,30 @@ const state = {
   
 };
 
+// Mutations
+const mutations = {
+
+  // Sets the state data for menuPostsData
+  'SET_LOCPOSTS' (state) {
+
+    // Test URL for the custom post type
+    var url = window.menu.protocol + '//' + window.menu.hostname + '/kog-champps-v2/wp-json/wp/v2/menu?filter[orderby]=date&per_page=100';
+
+    // Live URL for the custom post type
+    // var url = window.menu.protocol + '//' + window.menu.hostname + '/wp-json/wp/v2/menu?filter[orderby]=date&per_page=100';
+    
+    // Fetches the menu posts from WordPress and sets them to the menuPostsData array in the state
+    fetch(url).then(
+        (response)=>{
+        return response.json()
+    }).then((data)=>{
+        state.menuPostsData = data;
+    })
+            
+},
+
+}
+
 export default createStore({
   state,
   mutations,
