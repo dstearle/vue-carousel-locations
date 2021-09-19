06161ee61e4462ae52b5d21e1e90863aaf98c25a
dstearle <dallas.st.earle@gmail.com>
2021-09-19 14:20:06 -0700

    Tested state for wp cpt of 'menu'

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,39 @@
 
 <script>
 
-import Carousel from './components/Carousel.vue'
+import Carousel from './components/Carousel.vue';
+import { mapGetters, mapActions } from 'vuex';
+
 
 export default {
 
   name: 'App',
-  components: { Carousel }
+
+  components: { Carousel },
+
+  mounted() {
+    // Fetches all meta data for 'location' custom post type
+    this.fetchMenuPosts();
+  },
+
+  computed: {
+
+    ...mapGetters({
+        
+        // Retrieves the data for 'menu' custom post type
+        menuPostsDataGet: 'menuPostsDataGet',
+        
+    }),
+
+  },
+
+  methods: {
+
+    ...mapActions({
+        fetchMenuPosts: 'fetchMenuPosts',
+    }),
+
+  },
 
 }
 
